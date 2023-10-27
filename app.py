2c7ff4f0c8264653ed4da0eaef6f24821f3cd025
martin-creator <martinlubowa@outlook.com>
2023-10-27 20:06:21 +0300

    Added logic to create item in store

--- app.py
+++ app.py
@@ -29,3 +29,17 @@ def create_store():
     }
     stores.append(new_store)
     return jsonify(new_store), 201
+
+@app.route('/store/<string:name>', methods=['POST'])
+def create_item_in_store(name):
+    print(name)
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item), 201
+    return jsonify({'message': 'store not found'}), 404
