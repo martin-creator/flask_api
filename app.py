271d75cffc5a9b67b20f9e4fb36e639a16f97a24
martin-creator <martinlubowa@outlook.com>
2023-12-28 22:55:00 +0300

    Updated app.py

--- app.py
+++ app.py
@@ -1,115 +1,105 @@
+import os
 from flask import Flask, jsonify
-from flask_restful import Api
+from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 
 from db import db
 from blocklist import BLOCKLIST
-from resources.user import UserRegister, UserLogin, User, TokenRefresh, UserLogout
-from resources.item import Item, ItemList
-from resources.store import Store, StoreList
-from resources.tag import Tag
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["PROPAGATE_EXCEPTIONS"] = True
-db.init_app(app)
-migrate = Migrate(app, db)
-api = Api(app)
-
-"""
-JWT related configuration. The following functions includes:
-1) add claims to each jwt
-2) customize the token expired error message
-"""
-app.config["JWT_SECRET_KEY"] = "jose"
-jwt = JWTManager(app)
-
-"""
-`claims` are data we choose to attach to each jwt payload
-and for each jwt protected endpoint, we can retrieve these claims via `get_jwt_claims()`
-one possible use case for claims are access level control, which is shown below
-"""
+from resources.user import blp as UserBlueprint
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
 
 
-@jwt.additional_claims_loader
-def add_claims_to_jwt(identity):
-    # TODO: Read from a config file instead of hard-coding
-    if identity == 1:
-        return {"is_admin": True}
-    return {"is_admin": False}
-
-
-@jwt.token_in_blocklist_loader
-def check_if_token_in_blocklist(jwt_header, jwt_payload):
-    return jwt_payload["jti"] in BLOCKLIST
-
-
-@jwt.expired_token_loader
-def expired_token_callback(jwt_header, jwt_payload):
-    return jsonify({"message": "The token has expired.", "error": "token_expired"}), 401
-
-
-@jwt.invalid_token_loader
-def invalid_token_callback(error):
-    return (
-        jsonify(
-            {"message": "Signature verification failed.", "error": "invalid_token"}
-        ),
-        401,
+def create_app(db_url=None):
+    app = Flask(__name__)
+    load_dotenv()
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config[
+        "OPENAPI_SWAGGER_UI_URL"
+    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
+        "DATABASE_URL", "sqlite:///data.db"
     )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    api = Api(app)
 
+    app.config["JWT_SECRET_KEY"] = "jose"
+    jwt = JWTManager(app)
 
-@jwt.unauthorized_loader
-def missing_token_callback(error):
-    return (
-        jsonify(
-            {
-                "description": "Request does not contain an access token.",
-                "error": "authorization_required",
-            }
-        ),
-        401,
-    )
+    # @jwt.additional_claims_loader
+    # def add_claims_to_jwt(identity):
+    #     # TODO: Read from a config file instead of hard-coding
+    #     if identity == 1:
+    #         return {"is_admin": True}
+    #     return {"is_admin": False}
 
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
 
-@jwt.needs_fresh_token_loader
-def token_not_fresh_callback(jwt_header, jwt_payload):
-    return (
-        jsonify(
-            {"description": "The token is not fresh.", "error": "fresh_token_required"}
-        ),
-        401,
-    )
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
 
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
 
-@jwt.revoked_token_loader
-def revoked_token_callback(jwt_header, jwt_payload):
-    return (
-        jsonify(
-            {"description": "The token has been revoked.", "error": "token_revoked"}
-        ),
-        401,
-    )
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
 
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "The token is not fresh.",
+                    "error": "fresh_token_required",
+                }
+            ),
+            401,
+        )
 
-# JWT configuration ends
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked"}
+            ),
+            401,
+        )
 
+    api.register_blueprint(UserBlueprint)
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
 
-with app.app_context():
-    import models  # noqa: F401
-
-    db.create_all()
-
-
-api.add_resource(UserRegister, "/register")
-api.add_resource(UserLogin, "/login")
-api.add_resource(UserLogout, "/logout")
-api.add_resource(User, "/user/<int:user_id>")
-api.add_resource(TokenRefresh, "/refresh")
-api.add_resource(Store, "/store/<string:name>")
-api.add_resource(StoreList, "/store")
-api.add_resource(Item, "/item/<string:name>")
-api.add_resource(ItemList, "/item")
-api.add_resource(Tag, "/tag/<string:name>")
+    return app
