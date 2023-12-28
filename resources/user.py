5e4e1404105a7f283c84c1637d5547859eb4a9e9
martin-creator <martinlubowa@outlook.com>
2023-12-28 22:58:04 +0300

    Updated user resource

--- resources/user.py
+++ resources/user.py
@@ -1,5 +1,7 @@
-from flask import abort, request
-from flask_restx import Namespace, Resource, fields
+import os
+import redis
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
 from flask_jwt_extended import (
     create_access_token,
     create_refresh_token,
@@ -8,59 +10,66 @@ from flask_jwt_extended import (
     jwt_required,
 )
 from passlib.hash import pbkdf2_sha256
+from sqlalchemy import or_
+from rq import Queue
 
+from tasks import send_user_registration_email
+
+from db import db
 from models import UserModel
+from schemas import UserSchema, UserRegisterSchema
 from blocklist import BLOCKLIST
 
-api = Namespace("users", description="Operations related to users and authentication.")
-
-user_inputs = api.model(
-    "UserFields",
-    {
-        "username": fields.String(required=True),
-        "password": fields.String(required=True),
-    },
-)
-
-user_outputs = api.model("User", {"id": fields.String(), "username": fields.String()})
+blp = Blueprint("Users", "users", description="Operations on users")
+connection = redis.from_url(
+    os.getenv("REDIS_URL")
+)  # Get this from Render.com or run in Docker
+queue = Queue("emails", connection=connection)
 
 
-@api.route("/register")
-class UserRegister(Resource):
-    @api.expect(user_inputs, validate=True)
-    def post(self):
-        user_data = request.get_json()
-
-        if UserModel.find_by_username(user_data["username"]):
-            abort(400, "A user with that username already exists.")
+@blp.route("/register")
+class UserRegister(MethodView):
+    @blp.arguments(UserRegisterSchema)
+    def post(self, user_data):
+        if UserModel.query.filter(
+            or_(
+                UserModel.username == user_data["username"],
+                UserModel.email == user_data["email"],
+            )
+        ).first():
+            abort(409, message="A user with that username or email already exists.")
 
         user = UserModel(
             username=user_data["username"],
+            email=user_data["email"],
             password=pbkdf2_sha256.hash(user_data["password"]),
         )
-        user.save_to_db()
+        db.session.add(user)
+        db.session.commit()
+
+        queue.enqueue(send_user_registration_email, user.email, user.username)
 
         return {"message": "User created successfully."}, 201
 
 
-@api.route("/login")
-class UserLogin(Resource):
-    @api.expect(user_inputs, validate=True)
-    def post(self):
-        user_data = request.get_json()
-
-        user = UserModel.find_by_username(user_data["username"])
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(
+            UserModel.username == user_data["username"]
+        ).first()
 
         if user and pbkdf2_sha256.verify(user_data["password"], user.password):
             access_token = create_access_token(identity=user.id, fresh=True)
             refresh_token = create_refresh_token(user.id)
             return {"access_token": access_token, "refresh_token": refresh_token}, 200
 
-        abort(401, "Invalid credentials.")
+        abort(401, message="Invalid credentials.")
 
 
-@api.route("/logout")
-class UserLogout(Resource):
+@blp.route("/logout")
+class UserLogout(MethodView):
     @jwt_required()
     def post(self):
         jti = get_jwt()["jti"]
@@ -68,8 +77,8 @@ class UserLogout(Resource):
         return {"message": "Successfully logged out"}, 200
 
 
-@api.route("/user/<user_id>")
-class User(Resource):
+@blp.route("/user/<int:user_id>")
+class User(MethodView):
     """
     This resource can be useful when testing our Flask app.
     We may not want to expose it to public users, but for the
@@ -77,25 +86,25 @@ class User(Resource):
     when we are manipulating data regarding the users.
     """
 
-    @api.marshal_with(user_outputs)
-    def get(cls, user_id):
-        user = UserModel.find_by_id(user_id)
-        if not user:
-            abort(404, "User not found.")
-        return user, 200
+    @blp.response(200, UserSchema)
+    def get(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        return user
 
     def delete(self, user_id):
-        user = UserModel.find_by_id(user_id)
-        if not user:
-            abort(404, "User not found.")
-        user.delete_from_db()
+        user = UserModel.query.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()
         return {"message": "User deleted."}, 200
 
 
-@api.route("/refresh")
-class TokenRefresh(Resource):
+@blp.route("/refresh")
+class TokenRefresh(MethodView):
     @jwt_required(refresh=True)
     def post(self):
         current_user = get_jwt_identity()
         new_token = create_access_token(identity=current_user, fresh=False)
+        # Make it clear that when to add the refresh token to the blocklist will depend on the app design
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
         return {"access_token": new_token}, 200
